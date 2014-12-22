a4f8d88ca9d3f43c700fe7262495d1b8f9412be2
Panu Leppäniemi <me@panuleppaniemi.com>
2014-12-22 10:49:59 +0200

    Lusso

--- app/models/Business.scala
+++ app/models/Business.scala
@@ -2,11 +2,12 @@ package models
 
 case class Address(streetAddress: String)
 
+case class CustomerIdentifier(id: String)
+
 // Is there a better way for sum types or is this good enough?
 sealed trait Customer {
   val id: CustomerIdentifier
 }
-case class CustomerIdentifier(id: String)
 case class CustomerWithoutAddress(id: CustomerIdentifier) extends Customer
 case class CustomerWithAddress(id: CustomerIdentifier, address: Address) extends Customer
 
@@ -33,6 +34,11 @@ case class Visa(number: String) extends PaymentMethod
 case class MasterCard(number: String) extends PaymentMethod
 
 object Business {
+  type CreateAccount = (Username) => Option[Account]
+  type CreateCustomer = (CustomerIdentifier, Option[Address]) => Option[Customer]
+  type Register = CreateAccount => CreateCustomer
+
+  /*
   def createAccount(username: Username): Option[Account] = {
     Some(Account(username, "generatedPassword"))
   }
@@ -45,13 +51,14 @@ object Business {
       }
     )
   }
+   */
 
-  // Functional composition with typing? Kleislilosowski?
-  def register(username: String, customerIdentifier: String, address: String) = {
+  // Functional composition? Kleislilosowski? RWO?
+  /*def register(username: String, customerIdentifier: String, address: String) = {
     // Bind pattern?
     createAccount(Username(username)) match {
       case Some(account: Account) => createCustomer(CustomerIdentifier(customerIdentifier), Some(Address(address)))
       case None => false
     }
-  }
+  }*/
 }
